b7033f0227f61eb36f081726eab8080f6405a2d4
devadathsreelayam <devadath.r10@gmail.com>
2025-10-18 20:58:14 +0530

    Added: home page and doctor page

--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from hospital.models import Doctor
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Welcome to Hospital Management Application</h1>")
+
+def view_doctors(request):
+    doctors = Doctor.objects.all().select_related('user')
+    return render(request,'doctors.html', {'doctors': doctors})
