1cd84aedc4ae2486948663b7e73e109c27a3f712
devadathsreelayam <devadath.r10@gmail.com>
2025-10-18 20:57:26 +0530

    Models registered: User, Patient, Doctor.

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from hospital.models import Doctor, Patient, User
+
+
 # Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
