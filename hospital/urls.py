043aafa4fc83ade063dfee1cc8af3a92a2aa90aa
devadathsreelayam <devadath.r10@gmail.com>
2025-10-18 22:58:16 +0530

    Added HTML template and created views till appointment

--- hospital/urls.py
+++ hospital/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('doctors/', views.view_doctors, name='view_doctors'),
+    path('doctors/book/<int:doctor_id>/', views.make_appointment, name='make_appointment'),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -1,14 +1,47 @@
-from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import render
-from django.template import loader
 
 from hospital.models import Doctor
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Welcome to Hospital Management Application</h1>")
+    return render(request, 'home.html')
+
 
 def view_doctors(request):
     doctors = Doctor.objects.all().select_related('user')
-    return render(request,'doctors.html', {'doctors': doctors})
+
+    # Get filter parameters
+    specialization = request.GET.get('specialization', '')
+    search_query = request.GET.get('search', '')
+
+    # Apply filters
+    if specialization:
+        doctors = doctors.filter(specialization=specialization)
+
+    if search_query:
+        doctors = doctors.filter(
+            Q(user__first_name__icontains=search_query) |
+            Q(user__last_name__icontains=search_query) |
+            Q(specialization__icontains=search_query)
+        )
+
+    # Get specializations for filter dropdown
+    specializations = Doctor.SPECIALIZATION_CHOICES
+
+    context = {
+        'doctors': doctors,
+        'specializations': specializations,
+        'selected_specialization': specialization,
+        'search_query': search_query,
+    }
+    return render(request, 'doctors.html', context)
+
+
+def make_appointment(request, doctor_id):
+    doctor = Doctor.objects.get(pk=doctor_id)
+    context = {
+        'doctor': doctor
+    }
+    return render(request, 'appointment.html', context)
