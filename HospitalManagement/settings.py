4e966813a4baf1aaaa01c09fba6128979785be45
devadathsreelayam <devadath.r10@gmail.com>
2025-10-19 13:17:52 +0530

    Added: Login and logout feature for all users and corresponding dashboards

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'login'
 AUTH_USER_MODEL = 'hospital.User'
 # Application definition
 
--- hospital/views.py
+++ hospital/views.py
@@ -1,7 +1,12 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from hospital.models import Doctor
+from .forms import PatientRegistrationForm
 
 
 # Create your views here.
@@ -9,12 +14,69 @@ def index(request):
     return render(request, 'home.html')
 
 
-def login(request):
-    render(request, 'login.html')
+def login_view(request):
+    if request.user.is_authenticated:
+        return HttpResponse("Login Successfully")
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome back, {user.get_full_name()}!")
+
+            # Redirect based on user type
+            if user.user_type == 'patient':
+                print('Patient logged in')
+                return redirect('patient_dashboard')
+            elif user.user_type == 'doctor':
+                print('Doctor logged in')
+                return redirect('doctor_dashboard')
+            else:
+                return redirect('admin_dashboard')
+        else:
+            messages.error(request, f"Invalid Credentials!")
+            # return redirect('login')
+    return render(request, 'login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.success(request, f"You have been logged out successfully")
+    return redirect('index')
 
 
 def register(request):
     render(request, 'register.html')
+
+
+@login_required
+def dashboard(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == 'patient':
+            return redirect('patient_dashboard')
+        elif request.user.user_type == 'doctor':
+            return redirect('doctor_dashboard')
+        else:
+            return redirect('admin_dashboard')
+
+    return redirect('login')
+
+@login_required
+def patient_dashboard(request):
+    return render(request, 'patient_dash.html', {'user': request.user})
+
+
+@login_required
+def doctor_dashboard(request):
+    return render(request, 'doctor_dash.html', {'user': request.user})
+
+
+@login_required
+def admin_dashboard(request):
+    return render(request, 'admin_dash.html', {'user': request.user})
 
 
 def view_doctors(request):
